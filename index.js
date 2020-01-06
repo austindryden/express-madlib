73703ad85a176b2299dc75b383c06eeba74d40ec
Austin Dryden <austinisnorobot@gmail.com>
2020-01-06 14:17:35 -0500

    completes all assignments

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const http = require("http");
 const express = require("express");
+const fs = require('fs');
+const madHTML = fs.readFileSync("./madlib.html").toString();
 const app = express();
-const PORT = 3002;
 const server = http.createServer(app);
+
+const PORT = 3002;
+
 const VIPS = {
     'austin':"whaddup!",
     'chris': "yes?",
     'carly': "I'm sleepy",
-    'cat' "I'm a cat!":,
+    'cat': "I'm a cat!",
     'mark':"oh hi mark"
 };
 
-app.get('/:words',(request, response) =>{
-    const name = request.params.words;
-    if (VIPS.includes(name.toLowerCase())){
+console.log(madHTML);
+app.get('/:name/:place',(request, response) =>{
+    const name = request.params.name;
+    const place = request.params.place;
+    if (Object.keys(VIPS).includes(name.toLowerCase())){
         response.send(VIPS[name.toLowerCase()]);
     } else {
-        response.send(`hello, ${name}!`);
+        let nuMad = madHTML.replace("***NAME***", name);
+        nuMad = nuMad.replace("***PLACE***", place);
+        response.send(nuMad);
     }
 });
 
